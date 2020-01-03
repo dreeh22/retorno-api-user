47eadfd6fe5e5110d8c6563cc198aaa992000c4b
André de Jesus <andre.dejesus.9@gmail.com>
2020-01-03 16:56:06 -0300

    Adição do módulo de produção

--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { environment } from '../../environments/environment';
+import { environment } from '../../environments/environment.prod';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { DadosUsuario } from '../models/usuario';
@@ -13,11 +13,11 @@ export class UsuarioService {
   constructor(private http: HttpClient) { }
 
   getUsuariosALL(): Observable<DadosUsuario[]>{
-    return this.http.get<DadosUsuario[]>(environment.ApiURL + "/users")
+    return this.http.get<DadosUsuario[]>(environment.Api + "/users")
   }
 
   getPostUserId(userId: number): Observable<Post[]>{
-    return this.http.get<Post[]>(environment.ApiURL + "/posts?userId="+ userId)
+    return this.http.get<Post[]>(environment.Api + "/posts?userId="+ userId)
   }
 
 }
